ad25b14ee336bce2b6445c374b143cf7b1d190b8
Monica Giambitto <monica.giambitto@gmail.com>
2016-04-10 10:44:31 +0200

    remove warnings for unused variables

--- ch06/modules_and_functions_6.exs
+++ ch06/modules_and_functions_6.exs
@@ -1,5 +1,5 @@
 defmodule Chop do
-  def guess(actual, n, min..max) when actual > div(min + max, 2) do
+  def guess(actual, _, min..max) when actual > div(min + max, 2) do
     attempt = div(min + max, 2)
 
     IO.puts "Is it #{attempt}"
@@ -7,7 +7,7 @@ defmodule Chop do
     guess(actual, attempt, attempt..max)
   end
 
-  def guess(actual, n, min..max) when actual < div(min + max, 2) do
+  def guess(actual, _, min..max) when actual < div(min + max, 2) do
     attempt = div(min + max, 2)
 
     IO.puts "Is it #{attempt}"
@@ -15,7 +15,7 @@ defmodule Chop do
     guess(actual, attempt, min..attempt)
   end
 
-  def guess(actual, n, min..max), do: IO.puts actual
+  def guess(actual, _, _), do: IO.puts actual
 
   def guess(actual, min..max) when actual > 0 do
     attempt = div(min + max, 2)
