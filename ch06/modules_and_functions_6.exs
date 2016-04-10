fcde69ef5bc02a6bb10db7f757baa9594e94532f
Monica Giambitto <monica.giambitto@gmail.com>
2016-04-10 10:43:08 +0200

    remove double first print

--- ch06/modules_and_functions_6.exs
+++ ch06/modules_and_functions_6.exs
@@ -1,27 +1,27 @@
 defmodule Chop do
-  def verify(actual, n, min..max) when actual < div(min + max, 2) do
+  def guess(actual, n, min..max) when actual > div(min + max, 2) do
     attempt = div(min + max, 2)
 
     IO.puts "Is it #{attempt}"
 
-    verify(actual, attempt, min..attempt)
+    guess(actual, attempt, attempt..max)
   end
 
-  def verify(actual, n, min..max) when actual > div(min + max, 2) do
+  def guess(actual, n, min..max) when actual < div(min + max, 2) do
     attempt = div(min + max, 2)
 
     IO.puts "Is it #{attempt}"
 
-    verify(actual, attempt, attempt..max)
+    guess(actual, attempt, min..attempt)
   end
 
-  def verify(actual, n, min..max), do: IO.puts actual
+  def guess(actual, n, min..max), do: IO.puts actual
 
   def guess(actual, min..max) when actual > 0 do
     attempt = div(min + max, 2)
 
     IO.puts "Is it #{attempt}"
 
-    verify(actual, attempt, min..max)
+    guess(actual, attempt, min..attempt)
   end
 end
