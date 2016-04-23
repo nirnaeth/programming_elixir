7c0994fa03045a39eefbbd132cf45ffae66e2b22
Monica Giambitto <monica.giambitto@gmail.com>
2016-04-23 11:02:01 +0200

    cleaner attempt at string padding

--- ch11/strings_and_binaries_5.exs
+++ ch11/strings_and_binaries_5.exs
@@ -9,31 +9,12 @@ defmodule MyString do
     center(tail, justification)
   end
 
-  def center([head | tail]) do
-    current_max = String.length(head)
-    justification = calculate_justification(tail, current_max)
+  def center(words_list) do
+    max = words_list
+    |> Enum.max_by(&String.length(&1))
+    |> String.length
 
-    print_only_utf8(head, justification)
-
-    center(tail, justification)
-  end
-
-  defp calculate_justification([head | []], longest) do
-    current = String.length(head)
-    if current >= longest do
-      current
-    else
-      longest
-    end
-  end
-
-  defp calculate_justification([head | tail], longest) do
-    current = String.length(head)
-    if current >= longest do
-      calculate_justification(tail, current)
-    else
-      calculate_justification(tail, longest)
-    end
+    center(words_list, max)
   end
 
   defp print_only_utf8(string, justification) do
