3f3e822ebf1dfbf7ee09037d02cd0d3e41806049
Monica Giambitto <monica.giambitto@gmail.com>
2016-04-25 22:55:00 +0200

    second attempo, using binary pattern matching

--- ch11/strings_and_binaries_6.exs
+++ ch11/strings_and_binaries_6.exs
@@ -1,8 +1,19 @@
 defmodule MyString do
-  def capitalize_sentences(string) do
-    string
-    |> String.split(~r/\. /)
-    |> Enum.map(&(String.capitalize(&1)))
-    |> Enum.join(". ")
+  def capitalize_sentences(<< head :: utf8, tail :: binary >>) do
+    tail
+    |> String.downcase
+    |> do_capitalize_sentences(String.capitalize(<<head>>))
+  end
+
+  defp do_capitalize_sentences("", new_string), do: new_string
+
+  defp do_capitalize_sentences(<< ". ", tail :: binary >>, new_string) do
+    tail
+    |> String.capitalize
+    |> do_capitalize_sentences(Enum.join([new_string, ". "], ""))
+  end
+
+  defp do_capitalize_sentences(<< head :: utf8, tail :: binary >>, new_string) do
+    do_capitalize_sentences(tail, Enum.join([new_string, <<head>>], ""))
   end
 end
